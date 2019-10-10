9e0e1dcd9de307f21bcada8d17fa029aa793da38
Laurent Dupuis <box@dupuis.me>
2019-10-10 09:05:16 +0800

    Change to net45 and fix copyright infos

--- WellKnownLib/Properties/AssemblyInfo.cs
+++ WellKnownLib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WellKnownLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library to parse and generate OGC (Open Geospatial Consortium) Well-known text (WKT) and Well-known binary (WKB) representation of geometry")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Laurent Dupuis")]
 [assembly: AssemblyProduct("WellKnownLib")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Laurent Dupuis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
